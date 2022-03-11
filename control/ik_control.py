5de77b44f1230b60f2eea8910f60d9975cb0f2fe
kevin <shaowencheng@zju.edu.cn>
2022-03-11 20:23:17 +0800

    some control parameter

--- control/ik_control.py
+++ control/ik_control.py
@@ -72,13 +72,15 @@ class Controller():
         self.th1_joint_id = sim.model.joint_name2id('robot0:THJ2')
         self.th1_ac_id = sim.model.actuator_name2id('robot0:A_THJ2')
 
-    def apply_torque(self, joint_id, actuator_id, desired_pos, desired_vel, kp=100, kv=0.01 ):
+    def apply_torque(self, joint_id, actuator_id, desired_pos, desired_vel, fd_torque = 0, kp = 100, kv = 0.01 ):
         "pd controller"
         pos_torque = - kp * (self.sim.data.qpos[joint_id] - desired_pos)
         # print(self.sim.data.qpos[joint_id])
         vel_torque = - kv * (self.sim.data.qvel[joint_id] - desired_vel)
 
-        self.sim.data.ctrl[actuator_id] = pos_torque + vel_torque
+        self.sim.data.ctrl[actuator_id] = pos_torque + vel_torque + fd_torque
+        # if joint_id == self.ff_joint_id:
+        #     print('pos_torque', pos_torque, 'fd_torque', fd_torque)
 
 def main():
     data = np.load("functional grasp/data/data.npy")
@@ -94,72 +96,89 @@ def main():
     viewer.cam.distance = 2
     viewer.cam.azimuth = 180.
     viewer.cam.elevation = -30
+    viewer._paused = 1
     niter = 6999
-    # print(data)
     sensor_data=[]
+
+    # for i in range(niter):
+    #     hand.apply_torque(hand.xr_joint_id, hand.xr_ac_id, 0, 0)
+    #     hand.apply_torque(hand.zr_joint_id, hand.zr_ac_id, 0, 0)
+    #     hand.apply_torque(hand.xs_joint_id, hand.xs_ac_id, 0, 0)
+    #     hand.apply_torque(hand.ys_joint_id, hand.ys_ac_id, 0, 0)
+    #     hand.apply_torque(hand.zs_joint_id, hand.zs_ac_id, 0, 0, kp=10000)
+    #     hand.apply_torque(hand.th1_joint_id, hand.th1_ac_id, 0, 0)
+    #     hand.apply_torque(hand.ff_joint_id, hand.ff_ac_id, 0, 0)
+    #     hand.apply_torque(hand.mf_joint_id, hand.mf_ac_id, 0, 0)
+    #     hand.apply_torque(hand.rf_joint_id, hand.rf_ac_id, 0, 0)
+    #     hand.apply_torque(hand.lf_joint_id, hand.lf_ac_id, 0, 0)
+    #     hand.apply_torque(hand.th2_joint_id, hand.th2_ac_id, 0, 0)
+    #     sim.step()
+    #     viewer.render()
+
+    # print(sim.data.site_xpos)
+
     for i in range(1000):
-        hand.apply_torque(hand.xr_joint_id, hand.xr_ac_id, -1.76, 0, kp=100)
-        hand.apply_torque(hand.zr_joint_id, hand.zr_ac_id, 1.57, 0, kp=100)
-        hand.apply_torque(hand.xs_joint_id, hand.xs_ac_id, 0, 0, kp=100)
-        hand.apply_torque(hand.ys_joint_id, hand.ys_ac_id, -0.15, 0, kp=100)
+        hand.apply_torque(hand.xr_joint_id, hand.xr_ac_id, -1.76, 0)
+        hand.apply_torque(hand.zr_joint_id, hand.zr_ac_id, 1.57, 0)
+        hand.apply_torque(hand.xs_joint_id, hand.xs_ac_id, 0, 0)
+        hand.apply_torque(hand.ys_joint_id, hand.ys_ac_id, -0.15, 0)
         hand.apply_torque(hand.zs_joint_id, hand.zs_ac_id, 0, 0, kp=10000)
-        hand.apply_torque(hand.th1_joint_id, hand.th1_ac_id, data[11] / niter * i, 0, kp=20)
+        hand.apply_torque(hand.th1_joint_id, hand.th1_ac_id, data[11] / niter * i, 0)
         hand.apply_torque(hand.ff_joint_id, hand.ff_ac_id, 0, 0)
         hand.apply_torque(hand.mf_joint_id, hand.mf_ac_id, 0, 0)
         hand.apply_torque(hand.rf_joint_id, hand.rf_ac_id, 0, 0)
         hand.apply_torque(hand.lf_joint_id, hand.lf_ac_id, 0, 0)
         hand.apply_torque(hand.th2_joint_id, hand.th2_ac_id, 0, 0)
-        sensor_data.append(sim.data.sensordata.copy())
+        # sensor_data.append(sim.data.sensordata.copy())
         sim.step()
-        viewer.render()
+        # viewer.render()
 
     for i in range(niter + 1):
-        hand.apply_torque(hand.xs_joint_id, hand.xs_ac_id, data[0] / niter * i, 0, kp=1000)      
-        hand.apply_torque(hand.ys_joint_id, hand.ys_ac_id, -0.15 + (data[1] + 0.15) / niter * i, 0, kp=1000)
+        hand.apply_torque(hand.xs_joint_id, hand.xs_ac_id, data[0] / niter * i, 0)      
+        hand.apply_torque(hand.ys_joint_id, hand.ys_ac_id, -0.15 + (data[1] + 0.15) / niter * i, 0)
         hand.apply_torque(hand.zs_joint_id, hand.zs_ac_id, data[2] / niter * i, 0, kp=10000)
-        hand.apply_torque(hand.xr_joint_id, hand.xr_ac_id, -1.76 + (data[3] + 1.76)/ niter * i, 0, kp=1000)
+        hand.apply_torque(hand.xr_joint_id, hand.xr_ac_id, -1.76 + (data[3] + 1.76)/ niter * i, 0)
         hand.apply_torque(hand.yr_joint_id, hand.yr_ac_id, data[4] / niter * i, 0, kp=1000)
-        hand.apply_torque(hand.zr_joint_id, hand.zr_ac_id, 1.57 + (data[5] - 1.57)/ niter * i, 0, kp=1000)
+        hand.apply_torque(hand.zr_joint_id, hand.zr_ac_id, 1.57 + (data[5] - 1.57)/ niter * i, 0)
         hand.apply_torque(hand.ff_joint_id, hand.ff_ac_id, data[6] / niter * i, 0)
         hand.apply_torque(hand.mf_joint_id, hand.mf_ac_id, data[7] / niter * i, 0)
         hand.apply_torque(hand.rf_joint_id, hand.rf_ac_id, data[8] / niter * i, 0)
         hand.apply_torque(hand.lf_joint_id, hand.lf_ac_id, data[9] / niter * i, 0)
         hand.apply_torque(hand.th2_joint_id, hand.th2_ac_id, data[10] / niter * i, 0)
         hand.apply_torque(hand.th1_joint_id, hand.th1_ac_id, data[11], 0, kp=20)
-        sensor_data.append(sim.data.sensordata.copy())
         sim.step()
         viewer.render()
 
     for i in range(1000):
-        hand.apply_torque(hand.xs_joint_id, hand.xs_ac_id, data[0], 0, kp=100)      
-        hand.apply_torque(hand.ys_joint_id, hand.ys_ac_id, data[1], 0, kp=100)
-        hand.apply_torque(hand.zs_joint_id, hand.zs_ac_id, data[2], 0, kp=100)
-        hand.apply_torque(hand.xr_joint_id, hand.xr_ac_id, data[3], 0, kp=100)
-        hand.apply_torque(hand.yr_joint_id, hand.yr_ac_id, data[4], 0, kp=100)
-        hand.apply_torque(hand.zr_joint_id, hand.zr_ac_id, data[5], 0, kp=100)
+        hand.apply_torque(hand.xs_joint_id, hand.xs_ac_id, data[0], 0)      
+        hand.apply_torque(hand.ys_joint_id, hand.ys_ac_id, data[1], 0)
+        hand.apply_torque(hand.zs_joint_id, hand.zs_ac_id, data[2], 0)
+        hand.apply_torque(hand.xr_joint_id, hand.xr_ac_id, data[3], 0)
+        hand.apply_torque(hand.yr_joint_id, hand.yr_ac_id, data[4], 0)
+        hand.apply_torque(hand.zr_joint_id, hand.zr_ac_id, data[5], 0)
         hand.apply_torque(hand.ff_joint_id, hand.ff_ac_id, data[6], 0)
         hand.apply_torque(hand.mf_joint_id, hand.mf_ac_id, data[7], 0)
         hand.apply_torque(hand.rf_joint_id, hand.rf_ac_id, data[8], 0)
         hand.apply_torque(hand.lf_joint_id, hand.lf_ac_id, data[9], 0)
         hand.apply_torque(hand.th2_joint_id, hand.th2_ac_id, data[10], 0)
-        hand.apply_torque(hand.th1_joint_id, hand.th1_ac_id, data[11], 0, kp=20)
+        hand.apply_torque(hand.th1_joint_id, hand.th1_ac_id, data[11], 0)
         sensor_data.append(sim.data.sensordata.copy())
         sim.step()
         viewer.render()
 
     for i in range(5000):
-        hand.apply_torque(hand.xs_joint_id, hand.xs_ac_id, data[0], 0, kp=100)      
-        hand.apply_torque(hand.ys_joint_id, hand.ys_ac_id, data[1], 0, kp=100)
-        hand.apply_torque(hand.zs_joint_id, hand.zs_ac_id, data[2] + 0.00005 * i, 0, kp=100)
-        hand.apply_torque(hand.xr_joint_id, hand.xr_ac_id, data[3], 0, kp=100)
-        hand.apply_torque(hand.yr_joint_id, hand.yr_ac_id, data[4], 0, kp=100)
-        hand.apply_torque(hand.zr_joint_id, hand.zr_ac_id, data[5], 0, kp=100)
-        hand.apply_torque(hand.ff_joint_id, hand.ff_ac_id, data[6], 0)
-        hand.apply_torque(hand.mf_joint_id, hand.mf_ac_id, data[7], 0)
-        hand.apply_torque(hand.rf_joint_id, hand.rf_ac_id, data[8], 0)
-        hand.apply_torque(hand.lf_joint_id, hand.lf_ac_id, data[9], 0)
+        hand.apply_torque(hand.xs_joint_id, hand.xs_ac_id, data[0], 0)
+        hand.apply_torque(hand.ys_joint_id, hand.ys_ac_id, data[1], 0)
+        hand.apply_torque(hand.zs_joint_id, hand.zs_ac_id, data[2] + 0.0001 * i, 0)
+        hand.apply_torque(hand.xr_joint_id, hand.xr_ac_id, data[3], 0)
+        hand.apply_torque(hand.yr_joint_id, hand.yr_ac_id, data[4], 0)
+        hand.apply_torque(hand.zr_joint_id, hand.zr_ac_id, data[5], 0)
+        hand.apply_torque(hand.ff_joint_id, hand.ff_ac_id, data[6], 0, fd_torque= 0)
+        hand.apply_torque(hand.mf_joint_id, hand.mf_ac_id, data[7], 0, fd_torque= 0)
+        hand.apply_torque(hand.rf_joint_id, hand.rf_ac_id, data[8], 0, fd_torque= 0)
+        hand.apply_torque(hand.lf_joint_id, hand.lf_ac_id, data[9], 0, fd_torque= 0)
         hand.apply_torque(hand.th2_joint_id, hand.th2_ac_id, data[10], 0)
-        hand.apply_torque(hand.th1_joint_id, hand.th1_ac_id, data[11], 0, kp=20)
+        hand.apply_torque(hand.th1_joint_id, hand.th1_ac_id, data[11], 0, fd_torque= 0)
         sensor_data.append(sim.data.sensordata.copy())
         sim.step()
         viewer.render()
